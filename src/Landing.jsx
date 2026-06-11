029c9e3f06adac440d0a70b2b15da4ef212259fa
Jeva Varshini S <jevavarshini54@gamil.com>
2026-06-11 19:18:14 +0530

    Implemented animations for headers in Landing page

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,32 +1,41 @@
 import React from "react";
-import { easeOut, motion, scale, spring } from "framer-motion";
+import {motion} from "framer-motion";
 import "./Landing.css";
 
 function Landing({onselect}) {
 
     const itemVariants = {initial: {opacity: 0, y:20}, 
                           animate: {y:0, opacity: 1,
-                          transition: {duration: 2, ease: "easeOut"}} };
+                          transition: {duration: 0.5}}
+                         };
+    const landingVariants = {initial: {}, 
+                          animate: {transition: {staggerChildren: 0.5, delayChildren: 0.5}}
+                         };
 
-    const buttonHoverEffects = {
-                                    hover: {scale: 1.1, y: -7, backgroundColor: "black", color: "white", transition: {type: "spring", duration: 0.8, damping: 15, stiffness: 300}},
+    const buttonHoverEffects = {    initial: {opacity: 0, x:100},
+                                    animate: {opacity: 1, x:0, transition: {duration: 0.6}},
+                                    hover: {scale: 1.1, y: -7, transition: {type: "spring", duration: 0.8, damping: 15, stiffness: 300}},
                                     tap: {scale: 0.7, y: 0, transition: {type: "spring", stiffness: 500, damping: 25 }}
+    };
+
+    const btnContainer = {
+                            animate: {transition: {delayChildren: 0.5, staggerChildren: 0.5}}
     };
 
     return(
         <div className="landing_pg">
-            <div className="left_container">
-                <h1>WorkSphere</h1>
-                <h2>The speed of a system is entirely dictated by the visibility of its active node pipelines.</h2>
-                <h3>A centralized workspace architecture engineered to synchronize daily agendas, deliverables checklists, and performance updates across departments.</h3>
-            </div>
+            <motion.div className="left_container" variants={landingVariants} initial= "initial" animate="animate">
+                <motion.h1 variants={itemVariants}>WorkSphere</motion.h1>
+                <motion.h2 variants={itemVariants}>Work smarter, together.</motion.h2>
+                <motion.h3 variants={itemVariants}>Track projects, deadlines, and progress effortlessly.</motion.h3>
+            </motion.div>
 
             <div className="right_container"></div>
-            <div className="bottom_container">
-                <motion.button variants={buttonHoverEffects} whileHover="hover" whileTap="tap" onClick={() => onselect('dashboard')} className="tab_buttons">Dashboard</motion.button>
-                <motion.button variants={buttonHoverEffects} whileHover="hover" whileTap="tap" onClick={() => onselect('calendar')} className="tab_buttons">Calendar</motion.button>
-                <motion.button variants={buttonHoverEffects} whileHover="hover" whileTap="tap" onClick={() => onselect('todos')} className="tab_buttons">To Do</motion.button>
-            </div>
+            <motion.div className="bottom_container" variants={btnContainer}>
+                <motion.button variants={buttonHoverEffects} initial="initial" animate="animate" whileHover="hover" whileTap="tap" onClick={() => onselect('dashboard')} className="tab_buttons">Dashboard</motion.button>
+                <motion.button variants={buttonHoverEffects} initial="initial" animate="animate" whileHover="hover" whileTap="tap" onClick={() => onselect('calendar')} className="tab_buttons">Calendar</motion.button>
+                <motion.button variants={buttonHoverEffects} initial="initial" animate="animate" whileHover="hover" whileTap="tap" onClick={() => onselect('todos')} className="tab_buttons">To Do</motion.button>
+            </motion.div>
         </div>
     );
 } export default Landing;
