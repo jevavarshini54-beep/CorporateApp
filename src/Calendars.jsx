f54985f4b80a1657c9de3b15fc85338b34318c93
Jeva Varshini S <jevavarshini54@gamil.com>
2026-06-13 00:24:51 +0530

    Fixed loading and saving meeting details in calendars.jsx

--- src/Calendars.jsx
+++ src/Calendars.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Calendar from "react-calendar";
 import { useState } from "react";
 import './Calendars.css';
 
 function Calendars() {
+
+  const [meet, setMeet] = useState({});
   const [date, setDate] = useState(new Date());
+  const [loaded, setLoaded] = useState(false);
+
+  // To load meetings
+  useEffect(() =>{
+    const load = async() => {
+      try{
+        const result = await window.Storage.get("worksphereMeetings",true);
+        if (result?.value) setMeet (JSON.parse(result.value));
+      }
+
+      catch{}
+    };
+    load();
+  }, []);
+
+  // To save meetings
+  useEffect(() => {
+    if (!loaded) return;
+
+    const save = async() => {
+      try{
+        await window.Storage.set("worksphere-meetings", JSON.stringify(meet), true);
+      }
+
+      catch (e) {}
+    }
+    save();
+  }, [meet, loaded]);
+
   return (
     <div className="calendar_page">
       <div>
@@ -13,7 +44,6 @@ function Calendars() {
         )}/>
       </div>
       <div className="today">
-        <button onclick= {addMeeting}>Add Meeting</button>
       </div>
     </div>
   );
